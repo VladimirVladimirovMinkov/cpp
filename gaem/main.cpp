832e307ddaca78833d370aa4d9dbdd269d94eb69
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2023-01-21 14:06:10 +1300

    Update main.cpp

--- gaem/main.cpp
+++ gaem/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,59 +7,62 @@ class enemy{
     int health = 1;
     char symbol = 'e';
     int pos = 0;
+    float speed = 0.5;
+};
+
+class pos{
+public:
+    int x, y, point;
+    pos(int x, int y, int point){this->x = x, this->y = y, this->point = point;}
+    int gx(){return this->x;}
+    int gy(){return this->y;}
+    int gp(){return this->point;}
+};
+
+string mappath(string path){
+    int x = 0, y = 0;
+    int negshiftx = 0, negshifty = 0;
+    int rot = 0, crot = 0, l = 0;
+    vector<pos> points {};
+    points.push_back(pos(x, y, 0));
+    for (int i=1;i<(int(path.size())+1);i++) {
+        if (x < negshiftx) negshiftx = x;
+        if (y < negshifty) negshifty = y;
+        if (rot == 0){x+=1;}
+        if (rot == 1){y+=1;}
+        if (rot == 2){x-=1;}
+        if (rot == 3){y-=1;}
+        points.push_back(pos(x, y, i));
+        if (path[i] == ')') rot += 1;
+        if (path[i] == '(') rot -= 1;
+        if (rot > 3) rot = 0;
+        if (rot < 0) rot = 3;
+        if (crot == rot) {cout << rot << endl; l+=1;}
+        else {cout << rot << "," << l << endl; l=0;}
+        crot = rot;
+    }
+    string out;
+    for (int i=0;i<int(path.size());i++) {
+    out += "x " + to_string(points[i].gx()) + ", " + "y " + to_string(points[i].gy()) + "\n";}
+    // use two lists to make a dictionary like structure using vectors
+    cout << negshiftx << negshifty << endl;
+    return out;
 };
 
 int main()
 {
     // declare
-    string version = "0.1.0";
+    string version = "0.1.4";
     bool playing = true;
-    string path = "===(===)===)======(===(===)===";
-    string screen = "menu";
+    string path = "==(==)==)=====(==(==)==";
 
-    // string display
-    string main_menu =
-            "Gaem " + version +" test\n"
-            "new game\n"
-            "options\n"
-            "quit\n";
+    string map = mappath(path);
 
-    string game = "not made yet";
-
-    string options = "not made yet but might include difficulty or display options";
-
-    string display = main_menu;
-
-    int path_size = path.size();
-
-    for (int i;i<path_size;i++){
-
-    }
-
-    string option;
     while (playing) {
-        system("cls");
-        cout << screen << endl;
-        // display
-        cout << display << endl;
-
-        // play
-        getline(cin, option);
-        if (option == "quit") {
-            if (screen == "menu") playing = false;
-            if (screen == "game" or screen == "options") screen = "menu";
-        }
-        if (option == "options") screen = "options";
-        if (option == "new game") screen = "new game";
-
-        // process
-        if (screen == "menu") display = main_menu;
-
-        if (screen == "new game") display = game;
-
-        if (screen == "options") display = options;
-
+        cout << map << endl;
+        cin >> playing;
     }
+
     cout << endl << "Thank you for playing" << endl;
     return 0;
 }
